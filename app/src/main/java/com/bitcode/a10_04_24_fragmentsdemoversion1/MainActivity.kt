6060c3e615b5bc30170cfb459831b1ea418c5724
aishwarya <ashp17star@gmail.com>
2024-05-31 16:43:21 +0530

    Adding and Remove Fragments using array and fragment transaction

--- app/src/main/java/com/bitcode/a10_04_24_fragmentsdemoversion1/MainActivity.kt
+++ app/src/main/java/com/bitcode/a10_04_24_fragmentsdemoversion1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bitcode.a10_04_24_fragmentsdemoversion1
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private val fragmentManager = supportFragmentManager
     private lateinit var fragmentTransaction1 : FragmentTransaction
     private lateinit var fragmentTransaction2 : FragmentTransaction
+    private val counterFragments = ArrayList<CounterFragment>()
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,16 +29,26 @@ class MainActivity : AppCompatActivity() {
 
         btnAdd.setOnClickListener {
             //you can add fragment on button click
+            val counterFragment = CounterFragment()
             fragmentTransaction1 = fragmentManager.beginTransaction()
-            fragmentTransaction1.add(R.id.counterFragment,CounterFragment())
+            fragmentTransaction1.add(R.id.counterFragment,counterFragment)
             fragmentTransaction1.commit()
+            counterFragments.add(counterFragment)       //we need to add the fragment to arraylist
         }
 
         btnRemove.setOnClickListener {
             //you can remove fragment on button click
+            if(counterFragments.size == 0){
+                return@setOnClickListener
+            }
+            val counterFragmentIndex = counterFragments.size - 1
+            val counterFragment = counterFragments[counterFragmentIndex]    //calculating lastIndex
+            Log.e("tag","$counterFragmentIndex" +"")
             fragmentTransaction2 = fragmentManager.beginTransaction()
-            fragmentTransaction2.remove(CounterFragment())
+            fragmentTransaction2.remove(counterFragment)
             fragmentTransaction2.commit()
+
+            counterFragments.remove(counterFragment)    //we need to remove the fragment from an arraylist
         }
 //        counterFragment = supportFragmentManager.findFragmentById(R.id.counterFragment)
 //                as CounterFragment
